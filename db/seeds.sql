b5b82af0c2f9f2784aa225c54dcaa2725f7c1b1e
J <J21>
2018-01-06 13:23:59 -0600

    COMMIT

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burger, devoured, date) VALUES ('hamburger', true, 1/1/2017)
+INSERT INTO burgers (burger, devoured) VALUES ('hamburger', TRUE);
 
-INSERT INTO burgers (burger, devoured, date) VALUES ('cheeseburger', true, 1/2/2017);
+INSERT INTO burgers (burger, devoured) VALUES ('cheeseburger', TRUE);
 
-INSERT INTO burgers (burger, devoured, date) VALUES ('veggie burger', false, 1/3/2017);
+INSERT INTO burgers (burger, devoured) VALUES ('veggie burger', FALSE);
