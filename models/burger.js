055304d99a49f9df7f655667069f1af5a7e71a48
J <J21>
2018-02-04 16:48:07 -0600

    Commit

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,39 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
 
+//require ORM
+var orm = require('../config/orm.js');
+
+//object for export
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
+  //this function is unnecessary.
+  selectWhere: function(burgerName,callback){
+    orm.selectWhere('burgers','burger_name' ,burgerName,function(returnData){
+      callback(returnData)
+    }); 
+  }, //end selectWhere
+  insertOne: function(column,name,callback){
+    //use orm function(table,col,name,callback). 
+    orm.insertOne('burgers',column,name, function(returnData){
+      //callback is passed as parameter when burger.insertOne is called in other file.
+      callback(returnData);
     });
+  },//end insertOne
+  selectAll: function(callback){
+    //select all function used to load display with DB data from table burgers.
+    orm.selectAll('burgers',function(returnData){
+      callback(returnData);
+    });
+  },//end selectAll
+  updateOne: function(table,column,id,callback){
+    orm.updateOne('burgers',column, id,function(returnData){
+      callback(returnData);
+    })
   },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
+  deleteOne: function(table,id,callback){
+    orm.deleteOne('burgers',id,function(returnData){
+      callback(returnData);
+    })
   }
-};
+}//end burger
+
 
 module.exports = burger;
